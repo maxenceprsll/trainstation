c3df2b0d94cb1bc3e01c57613d19dd662d8e07da
maxenceprsll <maxence.persello@icloud.com>
2025-05-26 20:06:35 +0200

    Initial commit

---
+++ src/main/java/fr/maxencepersello/trainstation/TrainStationApplication.java
@@ -0,0 +1,13 @@
+package fr.maxencepersello.trainstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/maxencepersello/trainstation/TrainStationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.maxencepersello.trainstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
